fcda9044dab511181e6f62851f35892fea030878
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-05-07 10:03:40 -0600

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -6,7 +6,7 @@ CONSUMER_SECRET = "lAQlNXoaxyEequZHnitroGSyjOSlAxB6pc76qlIHyLMn1MTJTF"
 ACCESS_KEY = "1161775799596388352-f1e26ZYaJiL5XX7mX31l3t3VIggMPq"    
 ACCESS_SECRET = "ii8UUZ6wuUOgKd5NfMccvDDmzRO4sf41J53f3xq2eGiaH"
 
-num = random.randint(0,100)
+num = random.randint(0,32627)
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
